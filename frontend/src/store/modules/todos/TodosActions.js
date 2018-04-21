32fc46e6a74083eaa4ad9935783108f1813ec138
Alvaro J. Hurtado Villegas <alvaro.hurtado.villegas@gmail.com>
2018-04-21 08:51:42 +0200

    Move mutation names to contants.

--- frontend/src/store/modules/todos/TodosActions.js
+++ frontend/src/store/modules/todos/TodosActions.js
@@ -1,12 +1,14 @@
+import {ADD_TODO, REMOVE_TODO, TOGGLE_TODO} from "./TodosMutations";
+
 export default {
     addTodo({commit}, payload) {
         console.log(payload);
-        commit('ADD_TODO', payload)
+        commit(ADD_TODO, payload)
     },
     removeTodo({commit}, payload) {
-        commit('REMOVE_TODO', payload)
+        commit(REMOVE_TODO, payload)
     },
     toggleTodo({commit}, payload) {
-        commit('COMPLETE_TODO', payload)
+        commit(TOGGLE_TODO, payload)
     }
 }
--- frontend/src/store/modules/todos/TodosMutations.js
+++ frontend/src/store/modules/todos/TodosMutations.js
@@ -1,19 +1,23 @@
 import Vue from 'vue';
 
+export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO;';
+
 export default {
-        ADD_TODO(state, payload) {
+        [ADD_TODO](state, payload) {
             state.todos.push({
                 body: payload,
                 completed: false
             })
         },
 
-        REMOVE_TODO(state, payload) {
+        [REMOVE_TODO](state, payload) {
             var todos = state.todos
             todos.splice(todos.indexOf(payload), 1)
         },
 
-        TOGGLE_TODO(state, payload) {
+        [TOGGLE_TODO](state, payload) {
             payload.completed = !payload.completed
         },
 }

7696eb5355f42478d47fec11ff03d18d9bc52b43
Alvaro J. Hurtado Villegas <alvaro.hurtado.villegas@gmail.com>
2018-04-22 10:50:12 +0200

    Add counter element on toolbar.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,44 +3,47 @@
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
+      <todo-counter></todo-counter>
     </v-toolbar>
     <v-content>
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
           <v-layout column>
-            <todoForm></todoForm>
-            <TodoList></TodoList>
+            <todo-form></todo-form>
+            <todo-list></todo-list>
           </v-layout>
         </v-slide-y-transition>
       </v-container>
     </v-content>
     <v-footer :fixed="fixed" app>
-      <span>drupal&vuejs rocks!!!</span>
+      <span>drupal&vuejs rock!!!</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+    import TodoCounter  from './components/TodoCounter';
     import TodoList  from './components/TodoList';
     import TodoForm  from './components/TodoForm';
 
     export default {
       components: {
-          TodoForm,
-          TodoList,
+          'todo-form': TodoForm,
+          'todo-list': TodoList,
+          'todo-counter': TodoCounter,
       },
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'bubble_chart', title: 'Inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        title: 'Drupal VUEJS todo'
+      data () {
+        return {
+          clipped: false,
+          drawer: true,
+          fixed: false,
+          items: [
+            { icon: 'bubble_chart', title: 'Inspire' }
+          ],
+          miniVariant: false,
+          right: true,
+          title: 'Drupal VUEJS todo'
+        }
       }
-    }
   }
 </script>

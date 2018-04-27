64ffd46e162226200253650095f451a7a8f2cdb9
Alvaro Hurtado <alvaro.hurtado.villegas@gmail.com>
2018-04-27 07:59:59 +0200

    Mutations tweaks.

--- frontend/src/store/modules/todos/TodosMutations.js
+++ frontend/src/store/modules/todos/TodosMutations.js
@@ -5,28 +5,23 @@ export const REMOVE_TODO = 'REMOVE_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO;';
 
 export default {
-        [ADD_TODO](state, payload) {
-            state.todos.push({
-                id: payload.id,
-                body: payload.body,
-                completed: false
-            })
-        },
-
-        [REMOVE_TODO](state, payload) {
-            var index = -1;
-            for(var i = 0; i < state.todos.length; i++) {
-                if(state.todos[i].id == payload) {
-                    index = i;
-                }
+     [ADD_TODO](state, { id, body }) {
+        state.todos.push({ id, body, completed: false })
+     },
+    [REMOVE_TODO](state, id) {
+        var index = -1;
+        for(var i = 0; i < state.todos.length; i++) {
+            if(state.todos[i].id == id) {
+                index = i;
             }
-            if (index != -1) {
-                state.todos.splice(index, 1);
-            }
-        },
+        }
 
-        [TOGGLE_TODO](state, payload) {
-            let todo = state.todos.find(todo => todo.id === payload)
-            todo.completed = !todo.completed;
-        },
+        if (index != -1) {
+            state.todos.splice(index, 1);
+        }
+    },
+    [TOGGLE_TODO](state, id) {
+        let todo = state.todos.find(todo => todo.id === id)
+        todo.completed = !todo.completed;
+    },
 }

08b0345484706f38ecbf74850a1463ffefbcf88a
Alvaro J. Hurtado Villegas <alvaro.hurtado.villegas@gmail.com>
2018-04-29 19:02:58 +0200

    add services in order to make simpler run the example.

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-chip>
+        <v-avatar> {{this.count}}</v-avatar>
+        Pendientes
+    </v-chip>
+</template>
+
+<script>
+    export default {
+        computed: {
+            count() {
+                return this.$store.state.Todos.todos.filter(todo => todo.status === false).length;
+            }
+        }
+    }
+</script>

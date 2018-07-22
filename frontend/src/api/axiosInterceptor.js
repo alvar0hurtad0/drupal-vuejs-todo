7b38d3335a345490df82c666905830e6eaede1aa
Alvaro Hurtado <alvaro.hurtado.villegas@gmail.com>
2018-07-22 08:59:55 +0200

    Fix validation.

--- frontend/src/api/axiosInterceptor.js
+++ frontend/src/api/axiosInterceptor.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
 axios.interceptors.request.use(function(config) {
-    const auth_token = localStorage.getItem('auth_token');
-    if(auth_token) {
-      config.headers.Authorization = `Bearer ${auth_token}`;
+    const authToken = localStorage.getItem('auth_token');
+    const csrfToken = localStorage.getItem('csrf_token');
+    if(authToken && csrfToken) {
+      config.headers.common = {
+        "Authorization": `Basic ${authToken}`,
+        "CSRF-Token": csrfToken,
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      };
     }
     return config;
 }, function(err) {

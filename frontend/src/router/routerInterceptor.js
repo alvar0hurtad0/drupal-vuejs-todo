75d0be9c17b46f6ad6a8d42fa6733d45fc9459e6
Alvaro Hurtado <alvaro.hurtado.villegas@gmail.com>
2018-09-07 20:09:46 +0200

    Code Styles.

--- frontend/src/router/routerInterceptor.js
+++ frontend/src/router/routerInterceptor.js
@@ -1,14 +1,14 @@
 import router from '.'
 
 router.beforeEach((to,from,next) => {
-    //If visiting login view but you already have logged in, you should not be able to see this view.
+    // If visiting login view but you already have logged in redirect to home.
     if (to.name == 'Login' && window.localStorage.loggedIn) {
         return next({
             path: '/'
         })
     }
 
-    //Remove session on logout.
+    // Remove session on logout.
     if (to.name == 'Logout') {
         delete window.localStorage.loggedIn;
         return next({
@@ -16,7 +16,7 @@ router.beforeEach((to,from,next) => {
         })
     }
 
-    //If you are visiting a page that requires auth and you are a guest then, you must be redirected to login
+    // Redirect to login all request that need authentication if user is not logged.
     if(to.meta.requiresAuth &&  !(window.localStorage.loggedIn)){
         return next({
             path: '/login',
